e6cbe86c3fd3ec12b7b5b2261f406fb1431027f6
bjanos <jimi.bj.86@gmail.com>
2018-04-24 18:22:58 +0100

    add custom cell factory

--- src/app/ImageTextCell.java
+++ src/app/ImageTextCell.java
@@ -7,19 +7,31 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+/**
+ * List Cell class to customise List Cell Factory for the photos.
+ *
+ * @author Janos Benyovszki
+ * */
 public class ImageTextCell extends ListCell<Photo> {
     private HBox hBox = new HBox(8.0);
     private ImageView thumbImage = new ImageView();
     private Label label = new Label();
 
     public ImageTextCell() {
-        hBox.setAlignment(Pos.CENTER);
+        hBox.setAlignment(Pos.BASELINE_RIGHT);
+        hBox.setSpacing(8.0);
         thumbImage.setPreserveRatio(true);
-        //thumbImage.setFitHeight(50);
+        thumbImage.setFitHeight(50);
+
         hBox.getChildren().add(label);
         hBox.getChildren().add(thumbImage);
+
+        setPrefWidth(USE_PREF_SIZE);
     }
 
+    /**
+     * Customises cell visuals.
+     * */
     @Override
     protected void updateItem(Photo item, boolean empty) {
         super.updateItem(item, empty);
@@ -28,7 +40,8 @@ public class ImageTextCell extends ListCell<Photo> {
             setGraphic(null);
         } else {
             thumbImage.setImage(new Image(item.getImagePath()));
-            //label.setText(item.toString());
+            label.setText(item.toString());
+            setGraphic(hBox);
         }
     }
 }
